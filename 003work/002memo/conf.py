fcb1db6e0f9a40ad4f1f170fe9e67b1416c63697
kevinluolog <kevinluolog_72@163.com>
2019-11-05 16:40:08 +0800

    9

--- 003work/002memo/conf.py
+++ 003work/002memo/conf.py
@@ -148,6 +148,9 @@ latex_elements = {
 \usepackage{enumitem}
 \setlistdepth{99}
 ''',
+    'fontpkg': r'''
+\setCJKmainfont{Adobe Song Std}
+''',
     # Latex figure (float) alignment
     #
     # 'figure_align': 'htbp',
@@ -179,3 +182,79 @@ latex_elements = {
 ## Additional stuff for the LaTeX preamble.
 #'preamble': PREAMBLE,
 #}
+
+#中文字体
+#CJKmainfont: "FZShouJinShu-S10S"
+#CJKmainfont: "Microsoft YaHei"
+#CJKmainfont: "FZLiShu-S01S"
+#CJKmainfont: "FZLiShu-S01T"
+#CJKmainfont: "经典行书简"
+#CJKmainfont: "书体坊米芾体"
+#CJKmainfont: "经典隶书简"
+#CJKmainfont: "FZWeiBei-S03"
+#CJKmainfont: "FZShouJinShu-S10S"
+#CJKmainfont: "Adobe Fangsong Std"
+#CJKmainfont: "WenQuanYi Zen Hei"
+#FontType
+#fc-list -f "%{family}\n" :lang=zh > zhfont.txt
+
+#linux
+#  - sudo apt-get install ttf-wqy-microhei  #文泉驿-微米黑
+#  - sudo apt-get install ttf-wqy-zenhei  #文泉驿-正黑
+#  - sudo apt-get install xfonts-wqy #文泉驿-点阵宋体
+#`免费中文字体wiki.ubuntu.org.cn <https://wiki.ubuntu.org.cn/%E5%85%8D%E8%B4%B9%E4%B8%AD%E6%96%87%E5%AD%97%E4%BD%93>`__
+#$ fc-list :lang=zh-cn
+#/usr/share/fonts/truetype/wqy/wqy-microhei.ttc: WenQuanYi Micro Hei,文泉驛微米黑,文泉驿微米黑:style=Regular
+#/usr/share/fonts/X11/misc/wenquanyi_13px.pcf: WenQuanYi Bitmap Song:style=Regular
+#/usr/share/fonts/truetype/wqy/wqy-zenhei.ttc: WenQuanYi Zen Hei,文泉驛正黑,文泉驿正黑:style=Regular
+#/usr/share/fonts/X11/misc/wenquanyi_12pt.pcf: WenQuanYi Bitmap Song:style=Regular
+#/usr/share/fonts/truetype/wqy/wqy-zenhei.ttc: WenQuanYi Zen Hei Sharp,文泉驛點陣正黑,文泉驿点阵正黑:style=Regular
+#/usr/share/fonts/X11/misc/wenquanyi_10pt.pcf: WenQuanYi Bitmap Song:style=Regular
+#/usr/share/fonts/X11/misc/wenquanyi_9pt.pcf: WenQuanYi Bitmap Song:style=Regular
+#/usr/share/fonts/X11/misc/wenquanyi_11pt.pcf: WenQuanYi Bitmap Song:style=Regular
+#/usr/share/fonts/truetype/wqy/wqy-zenhei.ttc: WenQuanYi Zen Hei Mono,文泉驛等寬正黑,文泉驿等宽正黑:style=Regular
+
+# linux下：自己安装的字体，中文
+# Adobe Fangsong Std,Adobe 仿宋 Std,Adobe Fangsong Std R,Adobe 仿宋 Std R
+# Adobe Heiti Std,Adobe 黑体 Std,Adobe Heiti Std R,Adobe 黑体 Std R
+# Adobe Kaiti Std,Adobe 楷体 Std,Adobe Kaiti Std R,Adobe 楷体 Std R
+# Adobe Song Std,Adobe 宋体 Std,Adobe Song Std L,Adobe 宋体 Std L
+# FangSong,仿宋
+# FZFangSong-Z02S,方正仿宋简体
+# FZGuLi-S12T,方正古隶繁体
+# FZLiShu-S01S,方正隶书简体
+# FZLiShu-S01T,方正隶书繁体
+# FZShouJinShu-S10,方正瘦金书_GBK
+# FZShouJinShu-S10S,方正瘦金书简体
+# FZShouJinShu-S10T,方正瘦金书繁体
+# FZWeiBei-S03,方正魏碑_GBK
+# FZWeiBei-S03T,方正魏碑繁体
+# FZXiaoZhuanTi-S13T,方正小篆体
+# FZXingKai-S04T,方正行楷繁体
+# FZYingBiXingShu-S16S,方正硬笔行书简体
+# FZYingBiXingShu-S16T,方正硬笔行书繁体
+# KaiTi,楷体
+# Microsoft YaHei,微软雅黑
+# Microsoft YaHei,微软雅黑
+# NSimSun,新宋体
+# SimHei,黑体
+# SimSun,宋体
+# WenQuanYi Micro Hei Mono,文泉驛等寬微米黑,文泉驿等宽微米黑
+# WenQuanYi Micro Hei,文泉驛微米黑,文泉驿微米黑
+# WenQuanYi Zen Hei Mono,文泉驛等寬正黑,文泉驿等宽正黑
+# WenQuanYi Zen Hei Sharp,文泉驛點陣正黑,文泉驿点阵正黑
+# WenQuanYi Zen Hei,文泉驛正黑,文泉驿正黑
+# 经典繁行书
+# 经典行书简
+# 经典隶书简
+# 书体坊米芾体
+
+#windows下的引用名字
+#Microsoft YaHei,微软雅黑
+#FangSong,仿宋
+#Microsoft JhengHei,微軟正黑體
+#SimHei,黑体
+#NSimSun,新宋体
+#SimSun,宋体
+#KaiTi,楷体
+
